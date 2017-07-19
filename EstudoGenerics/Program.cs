0782a87e9693cd9ba47d2c86f9cdf9a7525a8b67
Haline <halineferreira@gmail.com>
2017-07-19 18:27:45 -0300

    No generics

--- EstudoGenerics/Program.cs
+++ EstudoGenerics/Program.cs
@@ -10,6 +10,33 @@ namespace EstudoGenerics
     {
         static void Main(string[] args)
         {
+            Pessoa pessoa = new Pessoa
+            {
+                Id = 1,
+                Nome = "Haline"
+            };
+
+            Animal animal = new Animal
+            {
+                Id = 1,
+                Especie = "Cachorro"
+            };
+
+            RepositorioPessoa repositorioPessoa = new RepositorioPessoa();
+            RepositorioAnimal repositorioAnimal = new RepositorioAnimal();
+
+            repositorioPessoa.Insert(pessoa);
+            repositorioAnimal.Insert(animal);
+
+            foreach (Pessoa p in repositorioPessoa.Get())
+            {
+                Console.WriteLine("Nome da pessoa: " + p.Nome);
+            }
+            foreach (Animal a in repositorioAnimal.Get())
+            {
+                Console.WriteLine("Especie do animal: " + a.Especie);
+            }
+            Console.ReadKey();
         }
     }
 }
